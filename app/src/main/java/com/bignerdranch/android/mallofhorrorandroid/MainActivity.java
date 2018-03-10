998582bf50a705b64a4ecb46f8430ebe9d407733
dexzhu1987 <dexzhu1987@hotmail.com>
2018-03-09 20:14:57 -0800

    updated local

--- app/src/main/java/com/bignerdranch/android/mallofhorrorandroid/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/mallofhorrorandroid/MainActivity.java
@@ -181,7 +181,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         gettingReady();
         updateRoom(MainActivity.this);
-
         displayMessage();
     }
 
